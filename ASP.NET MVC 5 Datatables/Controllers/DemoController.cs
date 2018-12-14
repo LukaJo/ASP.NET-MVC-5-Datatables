2c57d85960eb3de9c24a3c6bd4244df7b7834461
luka.jovic <luka.jovic93@gmail.com>
2018-12-14 14:03:36 +0100

    Add Edit and Delete options in Datatable

--- ASP.NET MVC 5 Datatables/Controllers/DemoController.cs
+++ ASP.NET MVC 5 Datatables/Controllers/DemoController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Linq.Dynamic;
+using System.Data.Entity;
 
 namespace ASP.NET_MVC_5_Datatables.Controllers
 {
@@ -67,6 +68,57 @@ namespace ASP.NET_MVC_5_Datatables.Controllers
                 throw;
             }
 
+        }
+
+        [HttpGet]
+        public ActionResult Edit(string ID)
+        {
+            try
+            {
+                using (DatabaseContext _context = new DatabaseContext())
+                {
+                    var Customer = (from customer in _context.Customers
+                                    where customer.CustomerID == ID
+                                    select customer).FirstOrDefault();
+
+                    return View(Customer);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Customers customers)
+        {
+            using (DatabaseContext _context = new DatabaseContext())
+            {
+                if (ModelState.IsValid)
+                {
+
+                    _context.Entry(customers).State = EntityState.Modified;
+                    _context.SaveChanges();
+                    return RedirectToAction("ShowGrid");
+                }
+                return View(customers);
+            }
+        }
+
+        [HttpPost]
+        public JsonResult DeleteCustomer(string ID)
+        {
+            using (DatabaseContext _context = new DatabaseContext())
+            {
+                var customer = _context.Customers.Find(ID);
+                if (ID == null)
+                    return Json(data: "Not Deleted", behavior: JsonRequestBehavior.AllowGet);
+                _context.Customers.Remove(customer);
+                _context.SaveChanges();
+
+                return Json(data: "Deleted", behavior: JsonRequestBehavior.AllowGet);
+            }
         }
 
 
